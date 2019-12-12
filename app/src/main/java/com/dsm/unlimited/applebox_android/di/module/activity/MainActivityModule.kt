f6e7b01de603ea7f0df911cdaaa039229c67d3b2
nwar-Jeon <nwar0825@naver.com>
2019-12-12 16:51:46 +0900

    [ADD] AAC Navigation

--- app/src/main/java/com/dsm/unlimited/applebox_android/di/module/activity/MainActivityModule.kt
+++ app/src/main/java/com/dsm/unlimited/applebox_android/di/module/activity/MainActivityModule.kt
@@ -1,8 +1,16 @@
 package com.dsm.unlimited.applebox_android.di.module.activity
 
+import com.dsm.unlimited.applebox_android.di.module.fragment.MainFragmentModule
+import com.dsm.unlimited.applebox_android.di.scope.ActivityScope
+import com.dsm.unlimited.applebox_android.di.scope.FragmentScope
+import com.dsm.unlimited.applebox_android.ui.fragment.MainFragment
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class MainActivityModule() {
+    @FragmentScope
+    @ContributesAndroidInjector(modules = [MainFragmentModule::class])
+    abstract fun mainFragment() : MainFragment
 
 }
--- app/src/main/java/com/dsm/unlimited/applebox_android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dsm/unlimited/applebox_android/ui/activity/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.dsm.unlimited.applebox_android.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dsm.unlimited.applebox_android.R
+import com.dsm.unlimited.applebox_android.databinding.ActivityMainBinding
+import com.dsm.unlimited.applebox_android.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override val layout: Int
+        get() = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
