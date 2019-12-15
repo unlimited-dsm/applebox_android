ffe73ef1490e17b3c666b693a4f793a72eaf61a8
nwar-Jeon <nwar0825@naver.com>
2019-12-15 13:38:08 +0900

    머지 X

--- app/src/main/java/com/dsm/unlimited/applebox_android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dsm/unlimited/applebox_android/ui/activity/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.dsm.unlimited.applebox_android.ui.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dsm.unlimited.applebox_android.R
-import com.dsm.unlimited.applebox_android.databinding.ActivityMainBinding
-import com.dsm.unlimited.applebox_android.ui.base.BaseActivity
-import com.dsm.unlimited.applebox_android.viewmodel.ActivityViewModel
-import com.dsm.unlimited.applebox_android.viewmodel.base.BaseViewModel
 import javax.inject.Inject
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
-
-    override val layout: Int
-        get() = R.layout.activity_main
-
-    @Inject
-    override lateinit var viewModel: ActivityViewModel
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
