51d9535e5928fd56f3846cf8481cdc84e9807f58
nwar-Jeon <nwar0825@naver.com>
2019-12-26 23:31:40 +0900

    [Add] NotifyFragment

---
+++ app/src/main/java/com/dsm/unlimited/applebox_android/viewmodel/NotifyViewModel.kt
@@ -0,0 +1,14 @@
+package com.dsm.unlimited.applebox_android.viewmodel
+
+import androidx.lifecycle.Lifecycle
+import com.dsm.unlimited.applebox_android.viewmodel.base.BaseViewModel
+
+class NotifyViewModel() : BaseViewModel() {
+    override fun apply(event: Lifecycle.Event) {
+
+    }
+
+    override fun clear() {
+
+    }
+}
