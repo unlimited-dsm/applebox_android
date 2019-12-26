1f2a9e0635b7938aeaa7f7cbad2d83f452978ed4
nwar-Jeon <nwar0825@naver.com>
2019-12-26 23:29:27 +0900

    [Add] NotifyFragment

--- app/src/main/java/com/dsm/unlimited/applebox_android/ui/fragment/MainProfileFragment.kt
+++ app/src/main/java/com/dsm/unlimited/applebox_android/ui/fragment/MainProfileFragment.kt
@@ -1,6 +1,9 @@
 package com.dsm.unlimited.applebox_android.ui.fragment
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dsm.unlimited.applebox_android.R
@@ -8,6 +11,7 @@ import com.dsm.unlimited.applebox_android.databinding.FragmentMainProfileBinding
 import com.dsm.unlimited.applebox_android.ui.adapter.MainProfileAdapter
 import com.dsm.unlimited.applebox_android.ui.base.BaseFragment
 import com.dsm.unlimited.applebox_android.viewmodel.MainProfileViewModel
+import org.jetbrains.anko.toolbar
 import javax.inject.Inject
 
 class MainProfileFragment() : BaseFragment<FragmentMainProfileBinding>() {
@@ -17,12 +21,23 @@ class MainProfileFragment() : BaseFragment<FragmentMainProfileBinding>() {
     @Inject
     override lateinit var viewModel: MainProfileViewModel
 
+    @Inject
+    lateinit var mainFragment: MainFragment
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
         binding.mainProfilePosterRecyclerview.apply {
             adapter = MainProfileAdapter(this@MainProfileFragment)
             layoutManager = LinearLayoutManager(this.context)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.findItem(R.id.main_profile_notify_iv).setOnMenuItemClickListener {
+            mainFragment.navigateToNotify()
+            true
         }
     }
 }
